d4f1aa45d708e56c2355bc9d99a0d662fa83dcf9
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-04-10 09:16:42 -0600

    runtimeanddatabasechanges

---
+++ brickwell2/Models/ViewModels/PaginationListViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace brickwell2.Models.ViewModels
+{
+    public class PaginationListViewModel
+    {
+        public IQueryable<Product>? Products { get; set; }
+        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo(); 
+
+        public string? CurrentItemType { get; set; }
+    }
+}
