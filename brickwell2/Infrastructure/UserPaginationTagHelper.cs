df8f4a45d78d360c941d551467cd39e536d53a29
Daxton Jergensen <daxjergy@byu.edu>
2024-04-10 15:40:24 -0600

    pagination for user page

---
+++ brickwell2/Infrastructure/UserPaginationTagHelper.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using brickwell2.Models.ViewModels;
+
+namespace brickwell2.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+
+    public class UserPaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public UserPaginationTagHelper(IUrlHelperFactory temp)
+        {
+            urlHelperFactory = temp;
+        }
+
+        [ViewContext] 
+        [HtmlAttributeNotBound] 
+        public ViewContext? ViewContext { get; set; }
+        public string? PageAction { get; set; }
+        public UserPagination PageModel { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext != null && PageModel != null)
+            {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+                TagBuilder result = new TagBuilder("div");
+                for (int i = 1; i <= PageModel.TotalNumPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+                    tag.InnerHtml.Append(i.ToString());
+
+                    result.InnerHtml.AppendHtml(tag);
+                }
+
+                output.Content.AppendHtml(result.InnerHtml);
+            }
+        }
+    }
+}
